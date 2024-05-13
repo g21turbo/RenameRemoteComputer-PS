60dbf4f0e753f0472cc4d97375beba185cd48382
g21turbo <dg21stew@gmail.com>
2024-05-12 22:02:51 -0700

    Contribution test

--- src/main/java/com/example/demoapp/HelloApplication.java
+++ src/main/java/com/example/demoapp/HelloApplication.java
@@ -3,7 +3,9 @@ package com.example.demoapp;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
@@ -11,10 +13,13 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("renameRemoteComputer.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 400, 275);
-        stage.setTitle("RenameRemotePC");
+        Scene scene = new Scene(fxmlLoader.load(), 375, 195);
+        stage.setTitle("Rename Remote Computer");
         stage.setScene(scene);
+        scene.setFill(Color.TRANSPARENT);
+
         stage.show();
+
     }
 
     public static void main(String[] args) {
